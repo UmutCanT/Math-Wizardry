09e36b95210daedaaeed0d8a7f4015eb36db5071
UmutCanT <uctekcambusiness@gmail.com>
2022-02-24 19:22:42 +0300

    Basic Question Generator

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    QuestionCreator questionCreator = new QuestionCreator();
+    MathQuestion mathQuestion;
+    List<MathQuestion> questions = new List<MathQuestion>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        questions.Add(questionCreator.QuestionGenerator(mathQuestion));
+        Debug.Log(questions[0].FirstNumber + " " + questions[0].MathOperation + " " + questions[0].SecondNumber + "=" + questions[0].CorrectAnswer);
     }
 
     // Update is called once per frame
--- Assets/Scripts/QuestionCreator.cs
+++ Assets/Scripts/QuestionCreator.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestionCreator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{ 
+    public MathQuestion QuestionGenerator(MathQuestion mathQuestion)
     {
-        
+        MathOperations operation = (MathOperations)Random.Range(0, 4);
+        Debug.Log("Hello2");
+        return operation switch
+        {
+            MathOperations.Addition => Addition(mathQuestion),
+            MathOperations.Substraction => Addition(mathQuestion),
+            MathOperations.Multiplication => Addition(mathQuestion),
+            MathOperations.Division => Addition(mathQuestion),
+            _ => Addition(mathQuestion),
+        };
     }
 
-    // Update is called once per frame
-    void Update()
+    MathQuestion Addition(MathQuestion mathQuestion)
     {
-        
+        mathQuestion.FirstNumber = Random.Range(0, 100);
+        mathQuestion.SecondNumber = Random.Range(0, 100);
+        mathQuestion.MathOperation = MathOperations.Addition;
+        mathQuestion.CorrectAnswer = mathQuestion.FirstNumber + mathQuestion.SecondNumber;
+        Debug.Log("Hello1");
+        return mathQuestion;
     }
 }
