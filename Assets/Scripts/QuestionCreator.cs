912631cad6d1e6a06d9bce0f6e39a8d61908191f
UmutCanT <uctekcambusiness@gmail.com>
2022-02-24 19:29:22 +0300

    Deleting Mono Behaviour on QC

--- Assets/Scripts/QuestionCreator.cs
+++ Assets/Scripts/QuestionCreator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionCreator : MonoBehaviour
+public class QuestionCreator
 { 
     public MathQuestion QuestionGenerator(MathQuestion mathQuestion)
     {
