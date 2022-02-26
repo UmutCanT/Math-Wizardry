662fb3afa397286306e27fb55035029491a0aecc
UmutCanT <uctekcambusiness@gmail.com>
2022-02-26 17:05:32 +0300

    Testing Assignment of Questions

---
+++ Assets/Scripts/AnswerCreator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerCreator : MonoBehaviour
+{
+    void RandomAnswerGenerator(int correctAnswer)
+    {
+
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     QuestionCreator questionCreator = new QuestionCreator();
     MathQuestion mathQuestion;
     List<MathQuestion> questions = new List<MathQuestion>();
+
+    //Part for testing
+    [SerializeField] Text problem;
+    [SerializeField] Button answer1;
+    [SerializeField] Button answer2;
+    [SerializeField] Button answer3;
+    [SerializeField] Button answer4;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +39,45 @@ public class GameManager : MonoBehaviour
 
     void AddingAndChecking()
     {
-        questions.Add(questionCreator.QuestionGenerator(mathQuestion));
-        CheckResults();
+        AssignProblem(questionCreator.QuestionGenerator(mathQuestion));
     }
+
+    void AssignProblem(MathQuestion question)
+    {
+        problem.text = string.Format("{0} {1} {2}", question.FirstNumber, OperationDecider(question.MathOperation), question.SecondNumber);
+    }
+
+    string OperationDecider(MathOperations operation)
+    {
+        return operation switch
+        {
+            MathOperations.Addition => "+",
+            MathOperations.Substraction => "-",
+            MathOperations.Multiplication => "*",
+            MathOperations.Division => "/",
+            _ => default
+        };
+    }
+
+
+    public void CheckAnswer(Text answer)
+    {
+        if (mathQuestion.CorrectAnswer == int.Parse(answer.text))
+        {
+            CorrectAnswer();
+        }
+        else
+            WrongAnswer();
+    }
+
+    void WrongAnswer()
+    {
+        Debug.Log("Wrong");
+    }
+
+    void CorrectAnswer()
+    {
+        Debug.Log("Correct");
+    }
+   
 }
